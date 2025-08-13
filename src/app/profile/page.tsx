d9d3a92f5ee4b0ef7045ed7a0c52836a7b40bfe7
najafsikander <najafsikander@yahoo.com>
2025-08-13 18:14:01 +0400

    Created basic layout for profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,14 +2,17 @@ import { checkUser } from "@/lib/checkUser";
 
 //TODO: Complete Profile Page. Profile pic, Avatar background, details, friends list, game tracker history.
 const ProfilePage = async () => {
-    const user = await checkUser();
+  const user = await checkUser();
 
-    if (!user) return (<h1>User isnt logged in</h1>)
-    return(
-        <section className="flex flex-row justify-center w-full h-screen">
-            <h1 className="mt-10">{user.fullName}</h1>
-        </section>
-    );
-}
+  if (!user) return <h1>User isnt logged in</h1>;
+  return (
+    <section className="flex flex-row justify-center w-full h-screen">
+      <section className="flex flex-row w-2/3 p-3 my-15 border rounded gap-2">
+      <aside className="basis-1/3 w-full bg-slate-600">{user.firstName}</aside>
+      <section className="basis-2/3 w-full bg-slate-400">{user.createdAt}</section>
+      </section>
+    </section>
+  );
+};
 
 export default ProfilePage;
