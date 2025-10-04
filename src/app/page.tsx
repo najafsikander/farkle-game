0edf317ad38eacd75ea8bf7186b73080bfe0d56f
najafsikander <najafsikander@yahoo.com>
2025-10-04 13:27:01 +0400

    Removed completed TODO

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import Hero from "./components/Hero";
-//TODO: Add Clerk implementation for account management
 //TODO: Add user friendship and search system
 export default function Home() {
   return (
