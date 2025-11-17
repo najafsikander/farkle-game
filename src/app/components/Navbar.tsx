2e4dd1f8d103eab75b0658004e72fdbf4131c3f7
najafsikander <najafsikander@yahoo.com>
2025-11-17 16:12:35 +0400

    Created debounce hook for search user input

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,19 +2,28 @@
 import { SignedIn, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 import { UserIcon } from "@heroicons/react/24/outline";
+import { useEffect, useState } from "react";
+import useDebounce from "../hooks/newDebounce";
 
 //TODO: Add User Search Option
 const Navbar: React.FC = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const debouncedSearchTerm = useDebounce(searchTerm, 500);
+
+  useEffect(() => {
+    console.log("Searching for user:", debouncedSearchTerm);
+  }, [debouncedSearchTerm]);
+  
   return (
     <>
       <div className="navbar bg-base-300">
         <div className="flex flex-row justify-between w-full">
-          <div className="flex gap-2">
+          <div className="flex gap-2 w-full">
             <Link className="btn btn-ghost text-xl" href="/">
               Farkle Game
             </Link>
             <SignedIn>
-              <input type="text" placeholder="Search User" className="input input-bordered w-full max-w-xs" list="users-list" id="search-user" name="search-user"/>
+              <input type="text" placeholder="Search User" className="input input-bordered w-full max-w-xs" list="users-list" id="search-user" name="search-user" onChange={(e) => setSearchTerm(e.target.value)}/>
               <datalist id="users-list">
                 <option value="user1"> user1 </option>
                 <option value="user2"> user2 </option>
